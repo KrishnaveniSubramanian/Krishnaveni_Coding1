28b9e3d2549bbb95c789105be2db6d7aba027e0d
KrishnaveniSubramanian <skrishnaveni826@gmail.com>
2025-11-09 00:43:58 +0530

    Add files via upload
    day1_DataType Assignment

---
+++ day1/ChromeDataTypes.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class ChromeDataTypes {
+
+	public static void main(String[] args) {
+		
+	//version of type float with a value of 91.0f 
+		
+	float version = 91.0f;
+	System.out.println(version +" is the float version");
+	
+	//developer of type String with a value of "Google" 
+	
+	String developer = "Google";
+	System.out.println(developer + " value with a type developer");
+	
+	//is Beta of type boolean with a value of false 
+	
+	boolean isBeta = false;
+	System.out.println("Beta value is " + isBeta);
+	
+	// releaseYear of type int with a value of 2008
+	
+	int releaseYear = 2008;
+	System.out.println(releaseYear +" is the releaseYear");
+	
+	
+	//shortcutKey of type char with a value of 'C' 
+	 
+	char shortcutKey = 'C';
+	System.out.println(shortcutKey + " is the shortcutKey");
+	
+	
+
+	}
+
+}
